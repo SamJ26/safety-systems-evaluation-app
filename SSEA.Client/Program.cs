2c79253c58667fb88b75e0cfa80f59633900e709
Samuel Janek <samueljanek17@gmail.com>
2021-01-24 15:04:57 +0100

    Prepared login component + generated client service

--- SSEA.Client/Program.cs
+++ SSEA.Client/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using SSEA.Client.BL.Services;
 
 namespace SSEA.Client
 {
@@ -18,6 +19,8 @@ namespace SSEA.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            
+            builder.Services.AddSingleton<IClientService, ClientService>();
 
             await builder.Build().RunAsync();
         }
