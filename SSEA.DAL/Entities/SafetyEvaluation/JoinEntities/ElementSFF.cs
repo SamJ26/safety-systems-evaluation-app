392a4c80b1aa9dd150c28dba669993137fcef2e4
Samuel Janek <samueljanek17@gmail.com>
2021-01-30 17:35:45 +0100

    Added join entities

---
+++ SSEA.DAL/Entities/SafetyEvaluation/JoinEntities/ElementSFF.cs
@@ -0,0 +1,16 @@
+﻿using SSEA.DAL.Entities.SafetyEvaluation.CodeListEntities.SIL_methodics;
+using SSEA.DAL.Entities.SafetyEvaluation.MainEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.DAL.Entities.SafetyEvaluation.JoinEntities
+{
+    public class ElementSFF : EntityBase
+    {
+        public int ElementId { get; set; }
+        public Element Element { get; set; }
+        public int SFF_Id { get; set; }
+        public SFF SFF { get; set; }
+    }
+}
