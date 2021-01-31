71574551f77dfd6ee67b2e7313b6d1e867c904fa
Samuel Janek <samueljanek17@gmail.com>
2021-01-31 17:06:42 +0100

    Prepared sketch of system entities

---
+++ SSEA.DAL/Entities/System/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SSEA.DAL.Entities.System
+{
+    [Table("SY_Entity")]
+    public class Entity : EntityBase
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public ICollection<State> States { get; set; }
+    }
+}
---
+++ SSEA.DAL/Entities/System/StateTransition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SSEA.DAL.Entities.System
+{
+    [Table("SY_StateTransition")]
+    public class StateTransition : EntityBase
+    {
+        [StringLength(200)]
+        public string Note { get; set; }
+
+        [Column("CurrentState_Id")]
+        public State CurrentState { get; set; }
+
+        [Column("NextState_Id")]
+        public State NextState { get; set; }
+    }
+}
