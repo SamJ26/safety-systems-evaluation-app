fe8c1194c212f1fbe5cc3835c579a4e2e2922daf
Samuel Janek <samueljanek17@gmail.com>
2021-02-05 11:08:11 +0100

    Prepared code list models for PL methodics

---
+++ SSEA.BL/Models/SafetyEvaluation/CodeListModels/PL_methodics/CategoryModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.BL.Models.SafetyEvaluation.CodeListModels.PL_methodics
+{
+    public class CategoryModel : ModelBase
+    {
+    }
+}
