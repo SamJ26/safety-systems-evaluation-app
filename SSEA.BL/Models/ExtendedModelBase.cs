6f352f4b8e683eec35afb4e01a144ad06133e8d9
Samuel Janek <samueljanek17@gmail.com>
2021-02-05 09:55:18 +0100

    Prepared main detail models

---
+++ SSEA.BL/Models/ExtendedModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.BL.Models
+{
+    public abstract class ExtendedModelBase : ModelBase
+    {
+        public int Id_created { get; set; }
+        public DateTime DT_created { get; set; }
+        public int? Id_updated { get; set; }
+        public DateTime? DT_updated { get; set; }
+    }
+}
---
+++ SSEA.BL/Models/ModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.BL.Models
+{
+    public abstract class ModelBase
+    {
+        public int Id { get; set; }
+    }
+}
