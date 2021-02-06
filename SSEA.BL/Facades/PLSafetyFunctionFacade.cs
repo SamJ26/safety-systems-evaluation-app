2ea88055476e958a08c8c8b7d1b2edb30c003d6a
Samuel Janek <samueljanek17@gmail.com>
2021-02-06 11:13:04 +0100

    Prepared PL safety function facade

---
+++ SSEA.BL/Facades/PLSafetyFunctionFacade.cs
@@ -0,0 +1,50 @@
+﻿using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
+using SSEA.DAL;
+using SSEA.DAL.Entities.SafetyEvaluation.MainEntities;
+using SSEA.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.BL.Facades
+{
+    public class PLSafetyFunctionFacade : IFacade<SafetyFunctionPL_DM, SafetyFunctionLM, SafetyFunction>
+    {
+        private readonly AppDbContext dbContext;
+        private readonly Repository<Machine> machineRepository;
+
+        // TODO: inject IMapper
+        public PLSafetyFunctionFacade(AppDbContext dbContext,
+                                      Repository<Machine> machineRepository)
+        {
+            this.dbContext = dbContext;
+            this.machineRepository = machineRepository;
+        }
+
+        public int Create(SafetyFunctionPL_DM newModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SafetyFunctionLM> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SafetyFunctionPL_DM GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(SafetyFunctionPL_DM updatedModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
