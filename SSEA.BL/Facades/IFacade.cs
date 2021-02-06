c8454110d2865a49715eff182a3912affc77f54f
Samuel Janek <samueljanek17@gmail.com>
2021-02-06 10:37:37 +0100

    Prepared interface for facades

---
+++ SSEA.BL/Facades/IFacade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.BL.Facades
+{
+    public interface IFacade<TDetailModel, TListModel>
+    {
+        IEnumerable<TListModel> GetAllAsync();
+        TDetailModel GetByIdAsync();
+        int CreateAsync(TDetailModel newModel);
+        int UpdateAsync(TDetailModel updatedModel);
+        void DeleteAsync(int Id);
+    }
+}
