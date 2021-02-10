4c04d97292efa37edba838991822dea98d7481c4
Samuel Janek <samueljanek17@gmail.com>
2021-02-10 11:41:01 +0100

    Prepared endpoints in machine controller

--- SSEA.Api/Controllers/MachineController.cs
+++ SSEA.Api/Controllers/MachineController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SSEA.BL.Facades;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,41 @@ namespace SSEA.Api.Controllers
         public MachineController(MachineFacade machineFacade)
         {
             this.machineFacade = machineFacade;
+        }
+
+        // api/machine
+        [HttpGet]
+        public ActionResult<IEnumerable<MachineListModel>> GetAll()
+        {
+            return Ok();
+        }
+
+        // api/machine/{id}
+        [HttpGet("{id}")]
+        public ActionResult<MachineDetailModel> GetById(int id)
+        {
+            return Ok();
+        }
+
+        // api/machine
+        [HttpPost]
+        public ActionResult<int> Create(MachineDetailModel newModel)
+        {
+            return Ok();
+        }
+
+        // api/machine
+        [HttpPut]
+        public ActionResult<int> Update(MachineDetailModel updatedModel)
+        {
+            return Ok();
+        }
+
+        // api/machine/{id}
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
         }
     }
 }
