1d0de8e6d4aa56a05a874251a72823124a495eb8
Samuel Janek <samueljanek17@gmail.com>
2021-02-10 11:50:20 +0100

    Prepared endpoints in safety function controller

--- SSEA.Api/Controllers/SafetyFunctionController.cs
+++ SSEA.Api/Controllers/SafetyFunctionController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SSEA.BL.Facades;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +19,62 @@ namespace SSEA.Api.Controllers
         public SafetyFunctionController(SafetyFunctionFacade safetyFunctionFacade)
         {
             this.safetyFunctionFacade = safetyFunctionFacade;
+        }
+
+        // api/safetyFunction
+        [HttpGet]
+        public ActionResult<IEnumerable<SafetyFunctionListModel>> GetAll()
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/pl/{id}
+        [HttpGet("pl/{id}")]
+        public ActionResult<SafetyFunctionDetailModelPL> GetByIdPL(int id)
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/sil/{id}
+        [HttpGet("sil/{id}")]
+        public ActionResult<SafetyFunctionDetailModelSIL> GetByIdSIL(int id)
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/pl
+        [HttpPost("pl")]
+        public ActionResult<int> Create(SafetyFunctionDetailModelPL newModel)
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/sil
+        [HttpPost("sil")]
+        public ActionResult<int> Create(SafetyFunctionDetailModelSIL newModel)
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/pl
+        [HttpPut("pl")]
+        public ActionResult<int> Update(SafetyFunctionDetailModelPL updatedModel)
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/sil
+        [HttpPut("sil")]
+        public ActionResult<int> Update(SafetyFunctionDetailModelSIL updatedModel)
+        {
+            return Ok();
+        }
+
+        // api/safetyFunction/{id}
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return Ok();
         }
     }
 }
