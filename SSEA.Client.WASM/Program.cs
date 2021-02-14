d68ad661c3d0656833654a81c6dd4cd2ed2a2edd
Samuel Janek <samueljanek17@gmail.com>
2021-02-14 17:16:14 +0100

    SSEA.Client project replaced with SSEA.Client.WASM with MudBlazor

--- SSEA.Client.WASM/Program.cs
+++ SSEA.Client.WASM/Program.cs
@@ -2,16 +2,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using SSEA.Client.BL.Services;
-using Blazored.LocalStorage;
-using Microsoft.AspNetCore.Components.Authorization;
 
-namespace SSEA.Client
+namespace SSEA.Client.WASM
 {
     public class Program
     {
@@ -22,11 +20,7 @@ namespace SSEA.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);
-            builder.Services.AddOptions();
-            builder.Services.AddAuthorizationCore();
-            builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProviderService>();
-            builder.Services.AddScoped<IClientService, ClientService>();
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
