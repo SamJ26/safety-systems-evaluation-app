4cb7c99604a276884f99c5bf86907affdcc3b02c
Samuel Janek <samueljanek17@gmail.com>
2021-02-22 15:25:19 +0100

    Added CodeListEntityBase

---
+++ SSEA.DAL/Entities/CodeListEntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.DAL.Entities
+{
+    public abstract class CodeListEntityBase : EntityBase, IExtendedEntityBase
+    {
+        public int IdCreated { get; set; }
+        public DateTime DateTimeCreated { get; set; }
+        public int? IdUpdated { get; set; }
+        public DateTime? DateTimeUpdated { get; set; }
+        public bool IsValid { get; set; }
+    }
+}
