92e1ba55d4e3c4f107f4417866a9c3718b49e67e
Samuel Janek <samueljanek17@gmail.com>
2021-02-27 11:22:33 +0100

    Added SSEA.BL.Tests project for business logic testing

---
+++ SSEA.BL.Tests/CodeListFacadeTest.cs
@@ -0,0 +1,25 @@
+using AutoMapper;
+using SSEA.BL.Facades;
+using SSEA.DAL;
+using SSEA.DAL.Tests;
+using System;
+using Xunit;
+
+namespace SSEA.BL.Tests
+{
+    public class CodeListFacadeTest
+    {
+        private readonly CodeListFacade codeListFacade;
+
+        public CodeListFacadeTest()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
