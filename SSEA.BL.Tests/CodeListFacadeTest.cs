1401d58b6fd863cfaa093ef4bf825c315fc50d40
Samuel Janek <samueljanek17@gmail.com>
2021-02-27 13:23:30 +0100

    Added GetAllCCFModels test

--- SSEA.BL.Tests/CodeListFacadeTest.cs
+++ SSEA.BL.Tests/CodeListFacadeTest.cs
@@ -1,25 +1,50 @@
 using AutoMapper;
 using SSEA.BL.Facades;
+using SSEA.BL.Models.SafetyEvaluation.CodeListModels.Common;
 using SSEA.DAL;
 using SSEA.DAL.Tests;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace SSEA.BL.Tests
 {
-    public class CodeListFacadeTest
+    public class CodeListFacadeTest : IDisposable
     {
-        private readonly CodeListFacade codeListFacade;
+        private readonly IMapper mapper;
+        private readonly DbContextInMemoryFactory dbContextFactory;
+        private AppDbContext dbContext;
 
         public CodeListFacadeTest()
         {
+            if (mapper == null)
+            {
+                var mapperConfig = new MapperConfiguration(mc =>
+                {
+                    mc.AddProfile(new CCFModelMapperProfile());
+                });
+                mapper = mapperConfig.CreateMapper();
+            }
+            dbContextFactory = new DbContextInMemoryFactory(nameof(AppDbContext));
+        }
 
+        public void Dispose()
+        {
+            dbContext?.Dispose();
         }
 
         [Fact]
-        public void Test1()
+        public async Task GetAllCCFModels()
         {
-
+            using (dbContext = dbContextFactory.CreateDbContext())
+            {
+                var facade = new CodeListFacade(mapper, dbContext);
+                var data = await facade.GetAllAsync("CCF");
+                Type dataType = ((object)data).GetType();
+                Assert.True(dataType == typeof(List<CCFModel>));
+                Assert.True(data.Count == 5);
+            }
         }
     }
 }
