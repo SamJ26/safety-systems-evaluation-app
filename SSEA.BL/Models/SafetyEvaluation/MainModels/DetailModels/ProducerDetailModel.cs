60cfcd841ddd73d5529d15c205e8890e05b2b774
Samuel Janek <samueljanek17@gmail.com>
2021-02-28 15:08:24 +0100

    Added ProducerDetailModelMapperProfile

--- SSEA.BL/Models/SafetyEvaluation/MainModels/DetailModels/ProducerDetailModel.cs
+++ SSEA.BL/Models/SafetyEvaluation/MainModels/DetailModels/ProducerDetailModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using SSEA.DAL.Entities.SafetyEvaluation.MainEntities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -13,5 +15,13 @@ namespace SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels
 
         [StringLength(50)]
         public string CountryOfOrigin { get; set; }
+    }
+
+    public class ProducerDetailModelMapperProfile : Profile
+    {
+        public ProducerDetailModelMapperProfile()
+        {
+            CreateMap<Producer, ProducerDetailModel>().ReverseMap();
+        }
     }
 }
