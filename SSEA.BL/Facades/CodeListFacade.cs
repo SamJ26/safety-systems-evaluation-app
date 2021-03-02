38a6cd688fd61efe87879684c16513d6f212c64e
Samuel Janek <samueljanek17@gmail.com>
2021-03-02 11:04:23 +0100

    Untracking entities in GetAll methods

--- SSEA.BL/Facades/CodeListFacade.cs
+++ SSEA.BL/Facades/CodeListFacade.cs
@@ -65,49 +65,50 @@ namespace SSEA.BL.Facades
         private async Task<ICollection<CCFModel>> GetAllCCFModels()
         {
             var repository = new Repository<CCF>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<CCFModel>>(data);
         }
 
         private async Task<ICollection<DCModel>> GetAllDCModels()
         {
             var repository = new Repository<DC>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<DCModel>>(data);
         }
 
         private async Task<ICollection<EvaluationMethodModel>> GetAllEvaluationMethodModels()
         {
             var repository = new Repository<EvaluationMethod>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<EvaluationMethodModel>>(data);
         }
 
         private async Task<ICollection<MachineTypeModel>> GetAllMachineTypeModels()
         {
             var repository = new Repository<MachineType>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<MachineTypeModel>>(data);
         }
 
         private async Task<ICollection<NormModel>> GetAllNormModels()
         {
             var repository = new Repository<Norm>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<NormModel>>(data);
         }
 
         private async Task<ICollection<TypeOfFunctionModel>> GetAllTypeOfFunctionModels()
         {
             var repository = new Repository<TypeOfFunction>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<TypeOfFunctionModel>>(data);
         }
 
         private async Task<ICollection<TypeOfLogicModel>> GetAllTypeOfLogicModels()
         {
             var repository = new Repository<TypeOfLogic>(dbContext);
-            var data = await repository.GetAll().Include(tol => tol.MaxArchitecture)
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking)
+                                                .Include(tol => tol.MaxArchitecture)
                                                 .Include(tol => tol.MaxPL)
                                                 .Include(tol => tol.MaxCategory)
                                                 .Include(tol => tol.MaxSIL)
@@ -118,14 +119,15 @@ namespace SSEA.BL.Facades
         private async Task<ICollection<TypeOfSubsystemModel>> GetAllTypeOfSubsystemModels()
         {
             var repository = new Repository<TypeOfSubsystem>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<TypeOfSubsystemModel>>(data);
         }
 
         private async Task<ICollection<CategoryModel>> GetAllCategoryModels()
         {
             var repository = new Repository<Category>(dbContext);
-            var data = await repository.GetAll().Include(c => c.MinMTTFd)
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking)
+                                                .Include(c => c.MinMTTFd)
                                                 .Include(c => c.MaxMTTFd)
                                                 .Include(c => c.MinDC)
                                                 .Include(c => c.MaxDC)
@@ -136,42 +138,42 @@ namespace SSEA.BL.Facades
         private async Task<ICollection<FModel>> GetAllFModels()
         {
             var repository = new Repository<F>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<FModel>>(data);
         }
 
         private async Task<ICollection<MTTFdModel>> GetAllMTTFdModels()
         {
             var repository = new Repository<MTTFd>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<MTTFdModel>>(data);
         }
 
         private async Task<ICollection<PLModel>> GetAllPLModels()
         {
             var repository = new Repository<PerformanceLevel>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<PLModel>>(data);
         }
 
         private async Task<ICollection<PModel>> GetAllPModels()
         {
             var repository = new Repository<P>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<PModel>>(data);
         }
 
         private async Task<ICollection<SModel>> GetAllSModels()
         {
             var repository = new Repository<S>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<SModel>>(data);
         }
 
         private async Task<ICollection<ArchitectureModel>> GetAllArchitectureModels()
         {
             var repository = new Repository<Architecture>(dbContext);
-            var data = await repository.GetAll().Include(a => a.MaxPFHd)
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).Include(a => a.MaxPFHd)
                                                 .ToListAsync();
             return mapper.Map<ICollection<ArchitectureModel>>(data);
         }
@@ -179,49 +181,49 @@ namespace SSEA.BL.Facades
         private async Task<ICollection<AvModel>> GetAllAvModels()
         {
             var repository = new Repository<Av>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<AvModel>>(data);
         }
 
         private async Task<ICollection<CFFModel>> GetAllCFFModels()
         {
             var repository = new Repository<CFF>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<CFFModel>>(data);
         }
 
         private async Task<ICollection<FrModel>> GetAllFrModels()
         {
             var repository = new Repository<Fr>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<FrModel>>(data);
         }
 
         private async Task<ICollection<PFHdModel>> GetAllPFHdModels()
         {
             var repository = new Repository<PFHd>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<PFHdModel>>(data);
         }
 
         private async Task<ICollection<PrModel>> GetAllPrModels()
         {
             var repository = new Repository<Pr>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<PrModel>>(data);
         }
 
         private async Task<ICollection<SeModel>> GetAllSeModels()
         {
             var repository = new Repository<Se>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<SeModel>>(data);
         }
 
         private async Task<ICollection<SFFModel>> GetAllSFFModels()
         {
             var repository = new Repository<SFF>(dbContext);
-            var data = await repository.GetAll().ToListAsync();
+            var data = await repository.GetAll().AsTracking(QueryTrackingBehavior.NoTracking).ToListAsync();
             return mapper.Map<ICollection<SFFModel>>(data);
         }
 
