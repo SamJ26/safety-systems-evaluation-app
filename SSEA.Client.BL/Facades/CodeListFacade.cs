f1390e8692fb403bc1bf8c9f1de8f78032a09bd8
Samuel Janek <samueljanek17@gmail.com>
2021-03-04 08:31:11 +0100

    Added CodeListFacade on frontend

---
+++ SSEA.Client.BL/Facades/CodeListFacade.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using SSEA.Client.BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSEA.Client.BL.Facades
+{
+    public class CodeListFacade
+    {
+        private readonly IClientService clientService;
+
+        public CodeListFacade(IClientService clientService)
+        {
+            this.clientService = clientService;
+        }
+
+        public async Task<dynamic> GetAllAsync(string typeName)
+        {
+            string json = await clientService.CodeListGetAllAsync(typeName);
+            dynamic data = JsonConvert.DeserializeObject<ExpandoObject>(json, new ExpandoObjectConverter());
+            return data;
+        }
+    }
+}
