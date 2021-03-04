222bf03d1b5bcbc16f54a4343020225889ad39a3
Samuel Janek <samueljanek17@gmail.com>
2021-03-04 08:20:27 +0100

    Added MachineFacade on frontend

---
+++ SSEA.Client.BL/Facades/MachineFacade.cs
@@ -0,0 +1,43 @@
+﻿using SSEA.Client.BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSEA.Client.BL.Facades
+{
+    public class MachineFacade
+    {
+        private readonly IClientService clientService;
+
+        public MachineFacade(IClientService clientService)
+        {
+            this.clientService = clientService;
+        }
+
+        public async Task<ICollection<MachineListModel>> GetAllAsync()
+        {
+            return await clientService.MachineGetAllAsync();
+        }
+
+        public async Task<MachineDetailModel> GetByIdAsync(int id)
+        {
+            return await clientService.MachineGetByIdAsync(id);
+        }
+
+        public async Task<int> CreateAsync(MachineDetailModel newModel)
+        {
+            return await clientService.MachineCreateAsync(newModel);
+        }
+
+        public async Task<int> UpdateAsync(MachineDetailModel updateModel)
+        {
+            return await clientService.MachineUpdateAsync(updateModel);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await clientService.MachineDeleteAsync(id);
+        }
+    }
+}
