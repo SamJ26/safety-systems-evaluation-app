bcf6f3b905994db8d52eaf72b0baffc532350a4a
Samuel Janek <samueljanek17@gmail.com>
2021-03-05 09:27:52 +0100

    Changed return type of GetAllAsync method to JArray

--- SSEA.Api/Controllers/CodeListController.cs
+++ SSEA.Api/Controllers/CodeListController.cs
@@ -9,6 +9,8 @@ using Swashbuckle.AspNetCore.Annotations;
 using SSEA.BL.Facades;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Text.Json;
+using SSEA.BL.Models.SafetyEvaluation.CodeListModels.Common;
 
 namespace SSEA.Api.Controllers
 {
@@ -32,12 +34,14 @@ namespace SSEA.Api.Controllers
         /// <returns> All records as JSON string </returns>
         [HttpGet("{typeName}")]
         [SwaggerOperation(OperationId = "CodeListGetAll")]
-        public async Task<ActionResult> GetAll(string typeName)
+        public async Task<ActionResult<JArray>> GetAll(string typeName)
         {
-            var data = await codeListFacade.GetAllAsync(typeName);
+            dynamic data = await codeListFacade.GetAllAsync(typeName);
             if (data == null)
                 return NotFound();
-            return Ok(data);
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            var jarray = JArray.Parse(json);
+            return Ok(jarray);
         }
 
         /// <summary>
