3dd4dcf53ffc4d96450a6a95e09a19ba4a4b0ad7
Samuel Janek <samueljanek17@gmail.com>
2021-03-13 18:32:32 +0100

    Added AccessPointFacade on backend

---
+++ SSEA.BL/Facades/AccessPointFacade.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
+using SSEA.DAL;
+using SSEA.DAL.Entities.SafetyEvaluation.MainEntities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSEA.BL.Facades
+{
+    public class AccessPointFacade : IFacade<AccessPointDetailModel, AccessPointListModel, AccessPoint>
+    {
+        private readonly AppDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public AccessPointFacade(AppDbContext dbContext, IMapper mapper)
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        // TODO: add GetAll method with filter
+
+        public Task<int> CreateAsync(AccessPointDetailModel newModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<AccessPointListModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AccessPointDetailModel> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(AccessPointDetailModel updatedModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
