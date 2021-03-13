c1c58592809344e6b897136b24138b74bd48531a
Samuel Janek <samueljanek17@gmail.com>
2021-03-13 18:22:55 +0100

    Adedd AccessPointFacade on frontend

---
+++ SSEA.Client.BL/Facades/AccessPointFacade.cs
@@ -0,0 +1,18 @@
+﻿using SSEA.Client.BL.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSEA.Client.BL.Facades
+{
+    public class AccessPointFacade
+    {
+        private readonly IClientService clientService;
+
+        public AccessPointFacade(IClientService clientService)
+        {
+            this.clientService = clientService;
+        }
+    }
+}
