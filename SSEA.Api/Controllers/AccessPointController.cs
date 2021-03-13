65f628a5b5fbc0cf2d3ce31f46b33a94d34aebaf
Samuel Janek <samueljanek17@gmail.com>
2021-03-13 18:33:09 +0100

    Prepared AccessPointController

---
+++ SSEA.Api/Controllers/AccessPointController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SSEA.BL.Facades;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
+using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSEA.Api.Controllers
+{
+    [Route("api/accessPoint")]
+    [ApiController]
+    public class AccessPointController : ControllerBase
+    {
+        private readonly AccessPointFacade accessPointFacade;
+
+        public AccessPointController(AccessPointFacade accessPointFacade)
+        {
+            this.accessPointFacade = accessPointFacade;
+        }
+
+        // Route: api/accessPoint
+        [HttpGet]
+        [SwaggerOperation(OperationId = "AccessPointGetAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ICollection<AccessPointListModel>>> GetAll()
+        {
+            var data = await accessPointFacade.GetAllAsync();
+            return Ok(data);
+        }
+
+        // Route: api/accessPoint/{id}
+        [HttpGet("{id}")]
+        [SwaggerOperation(OperationId = "AccessPointGetById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<AccessPointDetailModel>> GetById(int id)
+        {
+            var data = await accessPointFacade.GetByIdAsync(id);
+            if (data == null)
+                return NotFound();
+            return Ok(data);
+        }
+
+        // Route: api/accessPoint
+        [HttpPost]
+        [SwaggerOperation(OperationId = "AccessPointCreate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<int>> Create(AccessPointDetailModel newModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var id = await accessPointFacade.CreateAsync(newModel);
+            return Ok(id);
+        }
+
+        // Route: api/accessPoint
+        [HttpPut]
+        [SwaggerOperation(OperationId = "AccessPointUpdate")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<int>> Update(AccessPointDetailModel updatedModel)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var id = await accessPointFacade.UpdateAsync(updatedModel);
+            return Ok(id);
+        }
+
+        // Route: api/accessPoint/{id}
+        [HttpDelete("{id}")]
+        [SwaggerOperation(OperationId = "AccessPointDelete")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var foundId = await accessPointFacade.DeleteAsync(id);
+            if (foundId == 0)
+                return BadRequest();
+            return Ok();
+        }
+    }
+}
