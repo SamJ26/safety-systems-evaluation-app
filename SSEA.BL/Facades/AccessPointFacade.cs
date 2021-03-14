06f6d973c03a78bc1b9e1129a262932f4e70b190
Samuel Janek <samueljanek17@gmail.com>
2021-03-14 12:51:11 +0100

    Added implementation of AccessPointUpdateAsync

--- SSEA.BL/Facades/AccessPointFacade.cs
+++ SSEA.BL/Facades/AccessPointFacade.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using SSEA.BL.Models.SafetyEvaluation.JoinModels;
 using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
 using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
 using SSEA.DAL;
+using SSEA.DAL.Entities.SafetyEvaluation.JoinEntities;
 using SSEA.DAL.Entities.SafetyEvaluation.MainEntities;
 using System;
 using System.Collections.Generic;
@@ -50,9 +52,47 @@ namespace SSEA.BL.Facades
             return mapper.Map<AccessPointDetailModel>(data);
         }
 
-        public Task<int> UpdateAsync(AccessPointDetailModel updatedModel)
+        public async Task<int> UpdateAsync(AccessPointDetailModel updatedModel)
         {
-            throw new NotImplementedException();
+            // Items which have set Id are already in database and so can be removed for upcoming processing
+            foreach (var item in updatedModel.AccessPointSafetyFunctions)
+            {
+                // Item is already in database -> no operation with item
+                if (item.Id != 0)
+                    updatedModel.AccessPointSafetyFunctions.Remove(item);
+
+                // Safety function is already in database -> creating new record in join table
+                else if (item.SafetyFunctionId != 0)
+                    item.SafetyFunction = null;
+
+                // Safety function is new -> creating new safety function + record in join table
+                else if (item.SafetyFunctionId == 0 && item.SafetyFunction != null)
+                {
+                    // Inserting new safety function to databse
+                    var newSafetyFunction = mapper.Map<SafetyFunction>(item.SafetyFunction);
+                    dbContext.SafetyFunctions.Add(newSafetyFunction);
+                    await dbContext.SaveChangesAsync();
+
+                    // Creating new record in join table in database
+                    var newJoinTable = new AccessPointSafetyFunction()
+                    {
+                        AccessPointId = item.AccessPointId,
+                        SafetyFunctionId = newSafetyFunction.Id,
+                    };
+                    dbContext.AccessPointSafetyFunctions.Add(newJoinTable);
+                    await dbContext.SaveChangesAsync();
+
+                    // When item is processed it can be removed from collection
+                    updatedModel.AccessPointSafetyFunctions.Remove(item);
+                }
+            }
+
+            // In collection remains just items with AccessPointId and SafetyFunctionId
+            var entity = mapper.Map<AccessPoint>(updatedModel);
+            dbContext.AccessPoints.Update(entity);
+            await dbContext.SaveChangesAsync();
+
+            return entity.Id;
         }
     }
 }
