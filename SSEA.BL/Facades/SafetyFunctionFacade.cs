5e10edc78f3e0f7749988376a32aefbcbae5dbc8
Samuel Janek <samueljanek17@gmail.com>
2021-03-15 08:57:10 +0100

    Added GetAllMethod which returns safetyFunctions for specified AccessPoint

--- SSEA.BL/Facades/SafetyFunctionFacade.cs
+++ SSEA.BL/Facades/SafetyFunctionFacade.cs
@@ -3,15 +3,17 @@ using Microsoft.EntityFrameworkCore;
 using SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels;
 using SSEA.BL.Models.SafetyEvaluation.MainModels.ListModels;
 using SSEA.DAL;
+using SSEA.DAL.Entities.SafetyEvaluation.JoinEntities;
 using SSEA.DAL.Entities.SafetyEvaluation.MainEntities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SSEA.BL.Facades
 {
-    public class SafetyFunctionFacade : IExtendedFacade<SafetyFunctionDetailModelPL, SafetyFunctionDetailModelSIL, SafetyFunctionListModel, SafetyFunction>
+    public class SafetyFunctionFacade
     {
         private readonly AppDbContext dbContext;
         private readonly IMapper mapper;
@@ -27,10 +29,27 @@ namespace SSEA.BL.Facades
             var data = await dbContext.SafetyFunctions.Include(sf => sf.CurrentState)
                                                       .Include(sf => sf.EvaluationMethod)
                                                       .Include(sf => sf.TypeOfFunction)
+                                                      .AsNoTracking()
                                                       .ToListAsync();
             return mapper.Map<ICollection<SafetyFunctionListModel>>(data);
         }
 
+        public async Task<ICollection<SafetyFunctionListModel>> GetAllAsync(int accessPointId)
+        {
+            // Getting all safety functions which are related to selected access point
+            int[] safetyFunctions = await dbContext.AccessPointSafetyFunctions.Where(a => a.AccessPointId == accessPointId)
+                                                                              .Select(a => a.AccessPointId)
+                                                                              .ToArrayAsync();
+
+            // Filtering safety functions according to id
+            var data = await dbContext.SafetyFunctions.Include(sf => sf.CurrentState)
+                                                      .Include(sf => sf.EvaluationMethod)
+                                                      .Include(sf => sf.TypeOfFunction)
+                                                      .Where(sf => safetyFunctions.Contains(sf.Id))
+                                                      .ToListAsync();
+
+            return mapper.Map<ICollection<SafetyFunctionListModel>>(data);
+        }
 
         public Task<int> CreateAsync(SafetyFunctionDetailModelPL newModel)
         {
