db7278535c69e8e01c30114c0e6e5c7f32c5a8ba
Samuel Janek <samueljanek17@gmail.com>
2021-03-16 18:01:50 +0100

    Prepared first methods in SubsystemController

--- SSEA.Api/Controllers/SubsystemController.cs
+++ SSEA.Api/Controllers/SubsystemController.cs
@@ -22,5 +22,58 @@ namespace SSEA.Api.Controllers
             this.subsystemFacade = subsystemFacade;
         }
 
+        // GET: api/subsystem/pl/{id}
+        [HttpGet("pl/{id}")]
+        [SwaggerOperation(OperationId = "SubsystemGetAllPL")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ICollection<SubsystemDetailModelPL>>> GetAllPLAsync(int safetyFunctionId = 0)
+        {
+            ICollection<SubsystemDetailModelPL> data;
+            if (safetyFunctionId == 0)
+                data = await subsystemFacade.GetAllPLAsync();
+            else
+                data = await subsystemFacade.GetAllPLAsync(safetyFunctionId);
+            return Ok(data);
+        }
+
+        // GET: api/subsystem/sil/{id}
+        [HttpGet("sil/{id}")]
+        [SwaggerOperation(OperationId = "SubsystemGetAllSIL")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ICollection<SubsystemDetailModelSIL>>> GetAllSILAsync(int safetyFunctionId = 0)
+        {
+            ICollection<SubsystemDetailModelSIL> data;
+            if (safetyFunctionId == 0)
+                data = await subsystemFacade.GetAllSILAsync();
+            else
+                data = await subsystemFacade.GetAllSILAsync(safetyFunctionId);
+            return Ok(data);
+        }
+
+        // GET: api/subsystem/detail/pl/{id}
+        [HttpGet("detail/pl/{id}")]
+        [SwaggerOperation(OperationId = "SubsystemGetByIdPL")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<SubsystemDetailModelPL>> GetByIdPLAsync(int id)
+        {
+            var data = await subsystemFacade.GetByIdPLAsync(id);
+            if (data == null)
+                return NotFound();
+            return Ok(data);
+        }
+
+        // GET: api/subsystem/detail/sil/{id}
+        [HttpGet("detail/sil/{id}")]
+        [SwaggerOperation(OperationId = "SubsystemGetByIdSIL")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<SubsystemDetailModelSIL>> GetByIdSILAsync(int id)
+        {
+            var data = await subsystemFacade.GetByIdSILAsync(id);
+            if (data == null)
+                return NotFound();
+            return Ok(data);
+        }
     }
 }
