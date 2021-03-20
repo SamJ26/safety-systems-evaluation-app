967dea15cb8fda1e2697c643151e33cd2abb671c
Samuel Janek <samueljanek17@gmail.com>
2021-03-20 15:24:06 +0100

    Prepared new AuthenticationStateProvider

---
+++ SSEA.Client.WASM/Authentication/AuthStateProvider.cs
@@ -0,0 +1,50 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SSEA.Client.WASM.Authentication
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService storageService;
+        private readonly HttpClient httpClient;
+        private readonly AuthenticationState anonymous;
+
+        public AuthStateProvider(ILocalStorageService storageService, HttpClient httpClient)
+        {
+            this.storageService = storageService;
+            this.httpClient = httpClient;
+            anonymous = new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        }
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await storageService.GetItemAsync<string>("UserInfo");
+            if (string.IsNullOrWhiteSpace(token))
+                return anonymous;
+
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", token);
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token), "jwtAuthType")));
+        }
+
+        public void NotifyUserAuthentication(string email)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, email)
+            };
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(claims, "jwtAuthType"));
+            var authenticationState = Task.FromResult(new AuthenticationState(authenticatedUser));
+            NotifyAuthenticationStateChanged(authenticationState);
+        }
+
+        public void NotifyLogout()
+        {
+            var authenticationState = Task.FromResult(anonymous);
+            NotifyAuthenticationStateChanged(authenticationState);
+        }
+    }
+}
