a9b185c175a4808fa0bec13f861b3ccd484ecde7
Samuel Janek <samueljanek17@gmail.com>
2021-03-21 18:42:41 +0100

    Minor changes in machine frontend

--- SSEA.BL/Models/SafetyEvaluation/MainModels/DetailModels/MachineDetailModel.cs
+++ SSEA.BL/Models/SafetyEvaluation/MainModels/DetailModels/MachineDetailModel.cs
@@ -40,7 +40,7 @@ namespace SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels
         public TypeOfLogicModel TypeOfLogic { get; set; }
 
         public ICollection<AccessPointListModel> AccessPoints { get; set; }
-        public ICollection<MachineNormModel> MachineNorms { get; set; }
+        public ICollection<NormModel> Norms { get; set; }
     }
 
     public class MachineDetailModelMapperProfile : Profile
@@ -51,6 +51,7 @@ namespace SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels
                                                     .IgnoreSource(src => src.EvaluationMethodId)
                                                     .IgnoreSource(src => src.MachineTypeId)
                                                     .IgnoreSource(src => src.TypeOfLogicId)
+                                                    .Ignore(dest => dest.Norms)
                                                     .MapMember(dest => dest.DateTimeCreated, src => src.DateTimeCreated.ToString())
                                                     .MapMember(dest => dest.DateTimeUpdated, src => src.DateTimeUpdated.ToString())
                                                     .ReverseMap();
