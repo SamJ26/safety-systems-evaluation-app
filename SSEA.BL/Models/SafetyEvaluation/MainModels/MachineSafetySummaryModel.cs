a35050a8c723a54daf6385942d29f76c9ae08c94
Samuel Janek <samueljanek17@gmail.com>
2021-04-15 19:49:56 +0200

    Prepared models for machine summary

---
+++ SSEA.BL/Models/SafetyEvaluation/MainModels/MachineSafetySummaryModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSEA.BL.Models.SafetyEvaluation.MainModels
+{
+    public class MachineSafetySummaryModel
+    {
+        public int MachineId { get; set; }
+        public string MachineName { get; set; }
+        public StateModel MachineState { get; set; }
+        public string Message { get; set; }
+        public Dictionary<AccessPointInfo, List<SafetyFunctionInfo>> Summary { get; set; }
+    }
+
+    public class AccessPointInfo
+    {
+        public int AccessPointId { get; set; }
+        public string AccessPointName { get; set; }
+        public StateModel AccessPointState { get; set; }
+    }
+
+    public class SafetyFunctionInfo
+    {
+        public int SafetyFunctionId { get; set; }
+        public string SafetyFunctionName { get; set; }
+        public StateModel SafetyFunctionState { get; set; }
+    }
+}
