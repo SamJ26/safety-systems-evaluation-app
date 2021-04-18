be4bdc333d3103380791e68357c55fcf72d5fda4
Samuel Janek <samueljanek17@gmail.com>
2021-04-18 14:22:43 +0200

    Returned removed attributes to authentication models

--- SSEA.BL/Models/Auth/LoginUserModel.cs
+++ SSEA.BL/Models/Auth/LoginUserModel.cs
@@ -4,10 +4,12 @@ namespace SSEA.BL.Models.Auth
 {
     public class LoginUserModel
     {
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 1)]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 5)]
         public string Password { get; set; }
     }
--- SSEA.BL/Models/Auth/RegisterUserModel.cs
+++ SSEA.BL/Models/Auth/RegisterUserModel.cs
@@ -1,25 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SSEA.BL.Models.Auth
 {
     public class RegisterUserModel
     {
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 1)]
         public string FirstName { get; set; }
 
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 1)]
         public string LastName { get; set; }
 
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 1)]
         [EmailAddress]
         public string Email { get; set; }
 
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 5)]
         public string Password { get; set; }
 
+        [Required]
         [StringLength(maximumLength: 50, MinimumLength = 5)]
         public string ConfirmPassword { get; set; }
     }
