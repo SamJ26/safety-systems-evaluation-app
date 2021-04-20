6cf58fe5a1c37b0d3eb297141b5e43a4f2720c47
Samuel Janek <samueljanek17@gmail.com>
2021-04-20 20:48:43 +0200

    Created base class for element

---
+++ SSEA.BL/Models/SafetyEvaluation/MainModels/DetailModels/ElementDetailModel.cs
@@ -0,0 +1,12 @@
+﻿using SSEA.BL.Models.SafetyEvaluation.CodeListModels.Common;
+
+namespace SSEA.BL.Models.SafetyEvaluation.MainModels.DetailModels
+{
+    public abstract class ElementDetailModel : ExtendedModelBase
+    {
+        public string CatalogNumber { get; set; }
+        public double B10d { get; set; }
+        public ProducerModel Producer { get; set; }
+        public DCModel DC { get; set; }
+    }
+}
