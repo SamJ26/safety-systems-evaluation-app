266563782eb9af07ef3968e2044b19f9171de8a3
Samuel Janek <samueljanek17@gmail.com>
2021-04-24 15:07:11 +0200

    Prepared UserRepository

---
+++ SSEA.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSEA.DAL.Entities.Auth;
+using System.Threading.Tasks;
+
+namespace SSEA.DAL.Repositories
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public UserRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<string> GetUserNameById(int userId)
+        {
+            User user = await dbContext.Users.FirstOrDefaultAsync(u => u.Id == userId);
+            string userName = user.FirstName + " " + user.LastName;
+            return userName;
+        }
+    }
+}
